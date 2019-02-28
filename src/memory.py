4e4a666000d17a5afe770c8a201209ff18c6246c
yashadhikari <yashadhikari93@gmail.com>
2019-02-28 13:52:19 -0500

    Fixed the bug related to data creation with inner dependencies

--- src/memory.py
+++ src/memory.py
@@ -9,9 +9,14 @@ CS 535
 import random
 
 cycle = 1
+cache_miss = 0
+total_loads = 0
+
 def reset_cycle():
     global cycle
     cycle = 1
+    cache_miss = 0
+    total_loads = 0
 
 class Memory:
 
@@ -30,7 +35,7 @@ class Memory:
 
     def read(self, memory_address):
         global cycle
-
+        global cache_miss
         if self.noisy:
             input("cycle: {}".format(cycle))
         cycle += 1
@@ -38,6 +43,7 @@ class Memory:
         if self.current_delay > 0:
             return "wait"
         else:
+            cache_miss += 1
             start = memory_address >> self.bits_per_offset << self.bits_per_offset
             self.current_delay = self.initial_delay
             return self.data[start: start + self.words_per_read]
@@ -62,7 +68,7 @@ class Memory:
 
 class Cache:
     
-    def __init__(self, lines, associativity=2, words_per_line=4, delay=3, address_length=8, next_level=None, top_level=False, noisy=False):
+    def __init__(self, lines, associativity=1, words_per_line=4, delay=3, address_length=8, next_level=None, top_level=False, noisy=False):
         self.lines = lines
         self.words_per_line = words_per_line
         self.associativity = associativity
@@ -75,19 +81,18 @@ class Cache:
         self.current_delay = delay
         self.top_level = top_level
         self.noisy = noisy
-
-        self.data = [[0] * (words_per_line+1) + [0]] * lines
+        self.data = [[0] * (words_per_line+2) for line in range(lines)]
+        self.valid_bit_index = (words_per_line + 1)
         print("Cache created; {} lines, {} words per line; {} associativity".format(self.lines, self.words_per_line, self.associativity))
         print("{} bits per tag, {} bits per index, {} bits per offset".format(self.bits_per_tag, self.bits_per_index, self.bits_per_offset))
-
+        
     def get_data(self):
         return self.data
 
     def read(self, memory_address):
         global cycle
-        print("read() at delay", self.current_delay)
 
-        # The data is now in cache
+        # Wait until delay from current level cache is taken into account
         if self.noisy:
             input("cycle: {}".format(cycle))
         cycle += 1
@@ -105,10 +110,9 @@ class Cache:
             invalid_in_set = False
             row_location = None
             for row_index in range(start_index, end_index):
-                print("checking row with contents:", self.data[row_index], "at index", row_index)
                 
                 # If we haven't found an invalid row yet, and if the current row is invalid
-                if not invalid_in_set and self.data[row_index][5] == 0:
+                if not invalid_in_set and self.data[row_index][self.valid_bit_index] == 0:
                     first_invalid_row = row_index
                     invalid_in_set = True
                 
@@ -116,49 +120,30 @@ class Cache:
                 if self.data[row_index][0] == tag:
                     tag_in_set = True
                     row_location = row_index
-                    row_is_valid = (self.data[row_index][5] == 1)                    
-                    
-            print("row location", row_location)
-            print("tag in set?", tag_in_set, "invalid in set?", invalid_in_set)
+                    row_is_valid = (self.data[row_index][self.valid_bit_index] == 1)                    
                 
             # Cache miss - the tag is not in the set, or the tag is in set but invalid            
-            if not tag_in_set or not row_is_valid:          
-                print("cache miss.")
-    
+            if not tag_in_set or not row_is_valid:
                 response = "wait"
                 while response == "wait":
                     response = self.next_level.read(memory_address)
                 
-                # If the 
+                # If there is place to replace within the set
                 if invalid_in_set:
-                    print("there is an empty row in the set")
                     row_location = first_invalid_row
-                    # print(row_location)
                 else:
                     row_location = random.randrange(start_index, end_index)
-                    print("all rows in set are full. Putting at row", row_location)
-                    
-                print("before", self.data)
-                print("Updating row", row_location, "to have tag", tag)
-                print('\n', self.data[row_location][0])
-                print(self.data[row_location][1:5])
-                print(self.data[row_location][5], '\n')
-                self.data[row_location][0] = tag           # Update the tag
-                self.data[row_location][1:5] = response    # Update the cache
-                self.data[row_location][5] = 1             # Set the invalid bit to valid
-                print("after", self.data)
-
+                
+                self.data[row_location][0] = tag                                # Update the tag
+                self.data[row_location][1:self.words_per_line+1] = response     # Update the cache
+                self.data[row_location][self.valid_bit_index] = 1               # Set the invalid bit to valid
+                
             self.current_delay = self.initial_delay
             
             if self.top_level:
-                
-                # for debugging:
-                for row_index in range(start_index, end_index):
-                    print(self.data[row_index], row_index)
-                
                 return self.data[row_location][offset + 1]
             else:
-                return self.data[row_location][1:5]
+                return self.data[row_location][1:self.words_per_line+1]
 
     def parse_address(self, memory_address):
         tag = memory_address >> (self.address_length - self.bits_per_tag)
@@ -175,11 +160,20 @@ class Cache:
     def write(self, memory_address, value):
         global cycle
         tag, index, offset = self.parse_address(memory_address)
-
-        # If the data is in cache
-        if self.data[index][0] == tag:
-
-            # Then, write to cache first
+        start_index = index*self.associativity
+        end_index = start_index + self.associativity
+            
+        # Find whether or not the data is in cache and row location
+        tag_in_set = False
+        row_location = None
+        for row_index in range(start_index, end_index):
+            # Tag is in the set, but may be valid or invalid
+            if self.data[row_index][0] == tag:
+                tag_in_set = True
+                row_location = row_index
+            
+        # If cache hit, update cache first
+        if tag_in_set:
             if self.noisy:
                 input("cycle: {}".format(cycle))
             cycle += 1
@@ -187,9 +181,9 @@ class Cache:
             if self.current_delay > 0:
                 return "wait"
             else:
-                self.data[index][offset + 1] = value        # Update the cache
-                self.data[index][5] = 1                     # Set the invalid bit to valid
-                self.current_delay = self.initial_delay     # Reset the current delay
+                self.data[row_location][offset + 1] = value                     # Update the cache
+                self.data[row_location][self.valid_bit_index] = 1               # Set the invalid bit to valid
+                self.current_delay = self.initial_delay                         # Reset the current delay
 
         # In either case, write to memory
         response = "wait"
@@ -206,13 +200,16 @@ class MemoryDemo:
 
     def execute(self, instructions):
         global cycle
-
+        global cache_miss
+        global total_loads
+        
         for instruction in instructions:
 
             start_time = cycle
             arguments = instruction.split()
 
             if arguments[0] == "load":
+                total_loads += 1
                 response = self.load(int(arguments[1], 2))
                 print(instruction, "- value {:032b} loaded from address {} in {} cycles.".format(response, arguments[1], cycle - start_time))
             elif arguments[0] == "store":
@@ -220,7 +217,8 @@ class MemoryDemo:
                 print(instruction, "- value {} stored to address {} in {} cycles.".format(arguments[2], arguments[1], cycle - start_time))
 
             start_time = cycle
-
+        print("{:.2f} % of load instructions were a cache-miss".format(cache_miss/total_loads*100))
+        
     def load(self, memory_address):
         response = "wait"
         while response == "wait":
--- src/memory_demo.py
+++ src/memory_demo.py
@@ -19,17 +19,15 @@ from memory import Memory, Cache, MemoryDemo, reset_cycle
 
 def single_cache_demo():
     # Memory Demo
-    instructions = [
-        "load 0b01000000",                  # 13
-        "load 0b10000000",                  # 3 if associative, 13 if direct map
-        "load 0b00000000",                  # 3
-        "store 0b01101011 0b00000001",      # 10
-        "load 0b01101011",                  # 13
-        "store 0b01101011 0b00000001"       # 13
-    ]
+    instructions = []
+    with open('instructions.txt', 'r') as file: 
+        for line in file:
+            tag = line.rstrip('\n')
+            instructions.append(tag)
+    
 
     DRAM = Memory(lines=2**8, delay=10, noisy=False)
-    L1 = Cache(lines=8, words_per_line=4, delay=3, next_level=DRAM, top_level=True, noisy=False, associativity=2)
+    L1 = Cache(lines=8, words_per_line=2, delay=3, next_level=DRAM, top_level=True, noisy=False, associativity=2)
     demo = MemoryDemo(memory_heirarchy=[L1, DRAM])
     demo.execute(instructions)
 
@@ -37,18 +35,32 @@ def single_cache_demo():
 def two_cache_demo():
 
     # Memory Demo
-    instructions = [
-        "load 0b00000000",                  # 13
-        "load 0b00000000",                  # 3
-        "load 0b00000000",                  # 3
-        "store 0b01101011 0b00000001",      # 10
-        "load 0b01101011",                  # 13
-        "store 0b01101011 0b00000001"       # 13
-    ]
+    instructions = []
+    with open('instructions.txt', 'r') as file: 
+        for line in file:
+            tag = line.rstrip('\n')
+            instructions.append(tag)
 
-    DRAM = Memory(lines=2**8, delay=10, noisy=True)
-    L2 = Cache(lines=8, words_per_line=4, delay=3, next_level=DRAM, noisy=True)
-    L1 = Cache(lines=8, words_per_line=4, delay=3, next_level=L2, top_level=True, noisy=True)
+    DRAM = Memory(lines=2**8, delay=10, noisy=False)
+    L2 = Cache(lines=8, words_per_line=4, delay=3, next_level=DRAM, noisy=False)
+    L1 = Cache(lines=8, words_per_line=4, delay=3, next_level=L2, top_level=True, noisy=False)
+    demo = MemoryDemo(memory_heirarchy=[L1, L2, DRAM])
+    demo.execute(instructions)
+    
+def four_cache_demo():
+
+    # Memory Demo
+    instructions = []
+    with open('instructions.txt', 'r') as file: 
+        for line in file:
+            tag = line.rstrip('\n')
+            instructions.append(tag)
+
+    DRAM = Memory(lines=2**32, delay=10, noisy=False)
+    L4 = Cache(lines=64, words_per_line=4, delay=3, next_level=DRAM, noisy=False)
+    L3 = Cache(lines=32, words_per_line=4, delay=3, next_level=L4, noisy=False)
+    L2 = Cache(lines=16, words_per_line=4, delay=3, next_level=L3, noisy=False)
+    L1 = Cache(lines=8, words_per_line=4, delay=3, next_level=L2, top_level=True, noisy=False)
     demo = MemoryDemo(memory_heirarchy=[L1, L2, DRAM])
     demo.execute(instructions)
 
@@ -57,10 +69,15 @@ if __name__ == '__main__':
     print("*" * 20 + " ONE LEVEL CACHE DEMO" + "*" * 20)
     single_cache_demo()
     print("*"*50)
-
+    
     reset_cycle()
-    """
+    
     print("*" * 20 + " TWO LEVEL CACHE DEMO" + "*" * 20)
     two_cache_demo()
     print("*"*50)
-    """
+    
+    reset_cycle()
+    
+    print("*" * 20 + " FOUR LEVEL CACHE DEMO" + "*" * 20)
+    four_cache_demo()
+    print("*"*50)
