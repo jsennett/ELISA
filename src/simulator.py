9b56f8170b682ce0dc1c0626450b6e5f6d950e6a
Joshua Sennett <joshua.sennett@tufts.edu>
2019-03-14 17:50:30 -0400

    Added attributes to the simulator needed for the demo

--- src/simulator.py
+++ src/simulator.py
@@ -1,14 +1,21 @@
 import logging
+from memory import Memory, Cache
 
 class Simulator:
 
     def __init__(self):
-        self.pipeline = []
-        self.memory_heirarchy = None
-        self.cycle = 0
+        # Default values
+        self.pipeline = [None, None, None, None, None]
 
+        DRAM = Memory(lines=2**12, delay=100)
+        L1 = Cache(lines=8, words_per_line=1, delay=1, associativity=1, next_level=DRAM, name="L1")
+        L2 = Cache(lines=32, words_per_line=1, delay=1, associativity=1, next_level=L1, name="L2")
+        self.memory_heirarchy = [L1, L2, DRAM]
+        self.cycle = 0
         self.R = [0] * 32 # Integer registers
         self.F = [0] * 32 # Float registers
+        self.PC = 0
+        self.instructions = []
 
     def step(self):
         logging.info("Simulator: step()")
