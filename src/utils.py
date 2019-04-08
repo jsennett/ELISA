20168fdd6beda4e0d6534bf6c8208bbaf667e9b9
Joshua Sennett <joshua.sennett@tufts.edu>
2019-04-08 19:59:51 -0400

    Added docstrings

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 import struct
 
 def b_to_f(binary):
+    """Convert binary to floating point"""
     return struct.unpack('f', struct.pack('I', binary))[0]
 
 def f_to_b(decimal):
+    """Convert binary to floating point"""
     return struct.unpack('I', struct.pack('f', decimal))[0]
